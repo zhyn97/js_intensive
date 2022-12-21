1b0450a18e7bfdc05ce31b9908200f7d59b6f1ce
Nikita Zhyburtovich <n.zhyburtovich@gmail.com>
2022-12-21 19:57:52 +0100

    added html and js file

---
+++ index.js
@@ -0,0 +1,13 @@
+function validationNumber() {
+    let num1 = +prompt("Введите первое число");
+    let num2 = +prompt("Введите второе число");
+    if (isNaN(num1) || isNaN(num2) || num2 > 36 || num2 < 2) {
+      console.log("Некорректный ввод!");
+    } else {
+      const result = num1.toString(num2);
+      console.log(result);
+    }
+  }
+  
+  validationNumber();
+  
