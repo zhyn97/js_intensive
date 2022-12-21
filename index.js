886cd659aaf1c28e509796d67f54e5135019d111
Nikita Zhyburtovich <n.zhyburtovich@gmail.com>
2022-12-21 20:09:28 +0100

    finished task2

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-function validationNumber() {
+function calc() {
     let num1 = +prompt("Введите первое число");
     let num2 = +prompt("Введите второе число");
-    if (isNaN(num1) || isNaN(num2) || num2 > 36 || num2 < 2) {
+    if (isNaN(num1) || isNaN(num2)) {
       console.log("Некорректный ввод!");
+    } else if (num1 % num2 != 0) {
+      console.log(`${num1} не делится без остатка на ${num2}`);
     } else {
-      const result = num1.toString(num2);
-      console.log(result);
+        const sum = num1+num2;
+        const division = num1/num2
+        console.log(`Ответ: ${sum}, ${division}.`);
     }
   }
   
-  validationNumber();
+  calc();
   
