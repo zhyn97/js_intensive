e036679a77896a94c4f1911c5ca59dfbbcd02c81
Nikita Zhyburtovich <n.zhyburtovich@gmail.com>
2022-12-23 17:08:03 +0100

    completed interval task

--- index.js
+++ index.js
@@ -1,28 +1,65 @@
-function calc() {
-    let num1 = +prompt("Введите первое число");
-    let num2 = +prompt("Введите второе число");
-    if (isNaN(num1) || isNaN(num2)) {
-      console.log("Некорректный ввод!");
-    } else if (num1 % num2 != 0) {
-      console.log(`${num1} не делится без остатка на ${num2}`);
-    } else {
-        const sum = num1+num2;
-        const division = num1/num2
-        console.log(`Ответ: ${sum}, ${division}.`);
+// function calc() {
+//     let num1 = +prompt("Введите первое число");
+//     let num2 = +prompt("Введите второе число");
+//     if (isNaN(num1) || isNaN(num2)) {
+//       console.log("Некорректный ввод!");
+//     } else if (num1 % num2 != 0) {
+//       console.log(`${num1} не делится без остатка на ${num2}`);
+//     } else {
+//         const sum = num1+num2;
+//         const division = num1/num2
+//         console.log(`Ответ: ${sum}, ${division}.`);
+//     }
+//   }
+
+//   calc();
+
+// function validationNumber() {
+//   let num1 = +prompt("Введите первое число");
+//   let num2 = +prompt("Введите второе число");
+//   if (isNaN(num1) || isNaN(num2) || num2 > 36 || num2 < 2) {
+//     console.log("Некорректный ввод!");
+//   } else {
+//     const result = num1.toString(num2);
+//     console.log(result);
+//   }
+// }
+
+// validationNumber();
+
+function selectFromInterval(arr, intervalValue1, intervalValue2) {
+  if (
+    typeof intervalValue1 !== "number" ||
+    typeof intervalValue2 !== "number" ||
+    !Array.isArray(arr)
+  ) {
+    throw new Error("Переданное значение не является массивом");
+  }
+
+  for (i = 0; i <= arr.length - 1; i++) {
+    if (typeof arr[i] !== "number")
+      throw new Error("Одно или несколько значений в массиве не являются числом");
+  }
+
+  const intervalArray = [];
+
+  for (i = 0; i <= arr.length; i++) {
+    if (
+      (arr[i] >= intervalValue1 && arr[i] <= intervalValue2) ||
+      (arr[i] <= intervalValue1 && arr[i] >= intervalValue2)
+    ) {
+      intervalArray.push(arr[i]);
     }
   }
-  
-  calc();
-  
-function validationNumber() {
-  let num1 = +prompt("Введите первое число");
-  let num2 = +prompt("Введите второе число");
-  if (isNaN(num1) || isNaN(num2) || num2 > 36 || num2 < 2) {
-    console.log("Некорректный ввод!");
-  } else {
-    const result = num1.toString(num2);
-    console.log(result);
-  }
+
+  intervalArray.sort(function (a, b) {
+    return a - b;
+  });
+
+  return intervalArray;
 }
 
-validationNumber();
+function Error(message) {
+  this.message = message;
+}
+// Big O - O(n)
