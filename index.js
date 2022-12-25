e38487fbf2583ffc5987cbe39dbd4dbbd311b021
Nikita Zhyburtovich <n.zhyburtovich@gmail.com>
2022-12-25 02:12:56 +0100

    completed iterable object task

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
-function calc() {
-    let num1 = +prompt("Введите первое число");
-    let num2 = +prompt("Введите второе число");
-    if (isNaN(num1) || isNaN(num2)) {
-      console.log("Некорректный ввод!");
-    } else if (num1 % num2 != 0) {
-      console.log(`${num1} не делится без остатка на ${num2}`);
-    } else {
-        const sum = num1+num2;
-        const division = num1/num2
-        console.log(`Ответ: ${sum}, ${division}.`);
-    }
-  }
-  
-  calc();
-  
-function validationNumber() {
-  let num1 = +prompt("Введите первое число");
-  let num2 = +prompt("Введите второе число");
-  if (isNaN(num1) || isNaN(num2) || num2 > 36 || num2 < 2) {
-    console.log("Некорректный ввод!");
-  } else {
-    const result = num1.toString(num2);
-    console.log(result);
-  }
+// function calc() {
+//     let num1 = +prompt("Введите первое число");
+//     let num2 = +prompt("Введите второе число");
+//     if (isNaN(num1) || isNaN(num2)) {
+//       console.log("Некорректный ввод!");
+//     } else if (num1 % num2 != 0) {
+//       console.log(`${num1} не делится без остатка на ${num2}`);
+//     } else {
+//         const sum = num1+num2;
+//         const division = num1/num2
+//         console.log(`Ответ: ${sum}, ${division}.`);
+//     }
+//   }
+
+//   calc();
+
+// function validationNumber() {
+//   let num1 = +prompt("Введите первое число");
+//   let num2 = +prompt("Введите второе число");
+//   if (isNaN(num1) || isNaN(num2) || num2 > 36 || num2 < 2) {
+//     console.log("Некорректный ввод!");
+//   } else {
+//     const result = num1.toString(num2);
+//     console.log(result);
+//   }
+// }
+
+// validationNumber();
+
+function Error(mesage) {
+    this.mesage = mesage;
 }
 
-validationNumber();
+const myIterable = {
+  from: 2,
+  to: 10,
+};
+
+myIterable[Symbol.iterator] = function () {
+  if(typeof this.from !== 'number' || this.from > this.to){
+    throw new Error('Невалидные данные');
+  }
+  return {
+    current: this.from,
+    last: this.to,
+
+    next() {
+      if (this.current <= this.last) {
+        return { done: false, value: this.current++ };
+      } else {
+        return { done: true };
+      }
+    },
+  };
+};
+
+for (let item of myIterable) {
+  console.log(item);
+}
+//Big O - 0(n)
