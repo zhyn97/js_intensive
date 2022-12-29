c65c78b79972f78095de15d92e8290e628b40787
Nikita Zhyburtovich <n.zhyburtovich@gmail.com>
2022-12-29 19:31:20 +0100

    base methods added

--- index.js
+++ index.js
@@ -152,3 +152,121 @@ Array.prototype.myFilter = function (callback, thisArg) {
 
 const newArr = arr.myFilter(isPrime);
 console.log(newArr);
+
+//двусвязанный список
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null,
+            previous: null
+        };
+        this.length = 1;
+        this.tail = this.head;
+    }
+
+    append(value) {
+        let newNode = new Node(value);
+
+        this.tail.next = newNode;
+        newNode.previous = this.tail;
+        this.tail = newNode;
+
+        this.length++;
+        this.printList();
+    }
+
+    prepend(value) {
+        let newNode = new Node(value);
+
+        newNode.next = this.head;
+        this.head.previous = newNode;
+        this.head = newNode;
+
+        this.length++;
+        this.printList();
+    }
+
+    insert (index, value) {
+        if (!Number.isInteger(index) || index < 0 || index > this.length + 1) {
+            console.log(`Invalid index. Current length is ${this.length}.`);
+            return this;
+        }
+
+        if (index === 0) {
+            this.prepend(value);
+            return this;
+        }
+
+        if (index === this.length) {
+            this.append(value);
+            return this;
+        }
+
+        let newNode = new Node(value);
+        let previousNode = this.head;
+
+        for (let k = 0; k < index - 1; k++) {
+            previousNode = previousNode.next;
+        }
+
+        let nextNode = previousNode.next;
+        
+        newNode.next = nextNode;
+        previousNode.next = newNode;
+        newNode.previous = previousNode;
+        nextNode.previous = newNode;
+
+        this.length++;
+        this.printList();
+    }
+
+    remove (index) {
+        if (!Number.isInteger(index) || index < 0 || index > this.length) {
+            console.log(`Invalid index. Current length is ${this.length}.`);
+            return this;
+        }
+
+        if (index === 0) {
+            this.head = this.head.next;
+            this.head.previous = null;
+
+            this.length--;
+            this.printList();
+            return this;
+        }
+
+        if (index === this.length - 1) {
+            this.tail = this.tail.previous;
+            this.tail.next = null;
+
+            this.length--;
+            this.printList();
+            return this;
+        }
+
+        let previousNode = this.head;
+
+        for (let k = 0; k < index - 1; k++) {
+            previousNode = previousNode.next;
+        }
+        let deleteNode = previousNode.next;
+        let nextNode = deleteNode.next;
+
+        previousNode.next = nextNode;
+        nextNode.previous = previousNode;
+
+        this.length--;
+        this.printList();
+        return this;
+    }
+}
