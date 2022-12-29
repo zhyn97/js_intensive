be5c96fdff51290e5677622fe056862fe86fa422
Nikita Zhyburtovich <n.zhyburtovich@gmail.com>
2022-12-29 18:13:36 +0100

    merge with test branch

--- index.js
+++ index.js
@@ -1,5 +1,105 @@
-function test1 (a, b){
-    return a * b;
+function calc() {
+    let num1 = +prompt("Введите первое число");
+    let num2 = +prompt("Введите второе число");
+    if (isNaN(num1) || isNaN(num2)) {
+      console.log("Некорректный ввод!");
+    } else if (num1 % num2 != 0) {
+      console.log(`${num1} не делится без остатка на ${num2}`);
+    } else {
+        const sum = num1+num2;
+        const division = num1/num2
+        console.log(`Ответ: ${sum}, ${division}.`);
+    }
+  }
+
+
+function validationNumber() {
+  let num1 = +prompt("Введите первое число");
+  let num2 = +prompt("Введите второе число");
+  if (isNaN(num1) || isNaN(num2) || num2 > 36 || num2 < 2) {
+    console.log("Некорректный ввод!");
+  } else {
+    const result = num1.toString(num2);
+    console.log(result);
+  }
 }
 
-test1(2, 4);
+
+function selectFromInterval(arr, intervalValue1, intervalValue2) {
+  if (
+    typeof intervalValue1 !== "number" ||
+    typeof intervalValue2 !== "number" ||
+    !Array.isArray(arr)
+  ) {
+    throw new Error("Переданное значение не является массивом");
+  }
+
+  for (i = 0; i <= arr.length - 1; i++) {
+    if (typeof arr[i] !== "number")
+      throw new Error("Одно или несколько значений в массиве не являются числом");
+  }
+
+  const intervalArray = [];
+
+  for (i = 0; i <= arr.length; i++) {
+    if (
+      (arr[i] >= intervalValue1 && arr[i] <= intervalValue2) ||
+      (arr[i] <= intervalValue1 && arr[i] >= intervalValue2)
+    ) {
+      intervalArray.push(arr[i]);
+    }
+  }
+
+  intervalArray.sort(function (a, b) {
+    return a - b;
+  });
+
+  return intervalArray;
+}
+
+function Error(message) {
+  this.message = message;
+}
+
+
+function makeObjectDeepCopy(obj) {
+  let clone = Object.assign({}, obj);
+
+  Object.keys(clone).forEach(
+    (key) =>
+      (clone[key] =
+        typeof obj[key] === "object" ? makeObjectDeepCopy(obj[key]) : obj[key])
+  );
+
+  return Array.isArray(obj) && obj.length
+    ? (clone.length = obj.length) && Array.from(clone)
+    : Array.isArray(obj)
+    ? Array.from(obj)
+    : clone;
+}
+function Error(mesage) {
+    this.mesage = mesage;
+}
+
+const myIterable = {
+  from: 2,
+  to: 10,
+};
+
+myIterable[Symbol.iterator] = function () {
+  if(typeof this.from !== 'number' || this.from > this.to){
+    throw new Error('Невалидные данные');
+  }
+  return {
+    current: this.from,
+    last: this.to,
+
+    next() {
+      if (this.current <= this.last) {
+        return { done: false, value: this.current++ };
+      } else {
+        return { done: true };
+      }
+    },
+  };
+};
